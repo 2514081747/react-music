b8de49d25ba1e82e5da83ba27bafb364d722a99b
刘金财 <2514081747@qq.com>
2021-11-04 17:20:20 +0800

    no message

--- src/pages/discover/c-pages/ranking/index.js
+++ src/pages/discover/c-pages/ranking/index.js
@@ -1,9 +1,19 @@
 import React, { memo } from 'react'
+import {
+    RankWarpper,
+    RankLeft,
+    RankRight
+} from './style'
 
 export default memo(function LJranking() {
     return (
-        <div>
-            ljranking
-        </div>
+        <RankWarpper className='wrap-v2'>
+            <RankLeft>
+                aaaaaa
+            </RankLeft>
+            <RankRight>
+                aaaa
+            </RankRight>
+        </RankWarpper>
     )
 })
